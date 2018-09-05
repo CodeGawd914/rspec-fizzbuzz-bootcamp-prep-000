b00c5bcb348b54c4c54fe2323e3749e6ee6c2fa1
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-09-05 04:54:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if (int % 5 == 0) && (int % 3 == 0)
-   "Fizz"
+   "FizzBuzz"
   elsif int % 5 == 0
    "Buzz"
   elsif int % 3 == 0
-   "FizzBuzz"
-else  
+   "Fizz"
+else
 end
 end
